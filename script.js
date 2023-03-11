f4513e3b55e800a6f8c758630eb18e5a57965464
FreeMan <zelonaire@gmail.com>
2023-03-11 13:56:18 +0000

    an restuarant object

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Italy",
+  categories: ["Italian", "Pizzeria", "Vegeterian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMneu: ["Pizza", "Pasta", "Risotto"],
+};
+

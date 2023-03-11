41bdcf447307934f5f7f243b75b613590e40895f
FreeMan <zelonaire@gmail.com>
2023-03-11 14:31:59 +0000

    destructuring arrays

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const restaurant = {
   mainMneu: ["Pizza", "Pasta", "Risotto"],
 };
 
+//Traditional way of assigning a an element in an array to a variable
+//Example
+const arr = [1, 3, 4, 5]
+const a = arr[0]
+const b = arr[1];
+const c = arr[2];
+const d = arr[3];
+
+//By destructuring (starting from ES6)
+const [e, f, g, h] = arr
+//These new variables are assigned to the their corresponding element in the arr array 

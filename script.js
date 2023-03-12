6d712ae1f70181318b14effb74b2894f98c885b4
FreeMan <zelonaire@gmail.com>
2023-03-12 13:23:26 +0000

    destructuring arrays

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 "use strict";
 
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Italy",
-  categories: ["Italian", "Pizzeria", "Vegeterian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMneu: ["Pizza", "Pasta", "Risotto"],
-};
-
 //Traditional way of assigning a an element in an array to a variable
 //Example
 const arr = [1, 3, 4, 5]
@@ -18,4 +10,24 @@ const d = arr[3];
 
 //By destructuring (starting from ES6)
 const [e, f, g, h] = arr
-//These new variables are assigned to the their corresponding element in the arr array 
+//These new variables are assigned to the their corresponding element in the arr array
+
+//Destructuring objects
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Italy",
+  categories: ["Italian", "Pizzeria", "Vegeterian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMneu: ["Pizza", "Pasta", "Risotto"],
+};
+
+
+let [main, , sec] = restaurant.categories
+console.log(main, sec)
+
+const temp = main
+main = sec
+sec = temp
+console.log(main, temp)
+console.log(restaurant.categories)
